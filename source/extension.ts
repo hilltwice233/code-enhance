994b6e0c149d598dcf2a3090c185b19af384f9ef
hilltwice <hilltwice@163.com>
2023-11-03 17:01:53 +0800

    stable support git line commit message

--- source/extension.ts
+++ source/extension.ts
@@ -2,11 +2,6 @@ import * as vscode from "vscode"
 import {enableGitLineHistory} from "./git-line-history.js"
 
 export function activate(context: vscode.ExtensionContext) {
-  context.subscriptions.push(
-    vscode.commands.registerCommand("code-enhance.active", () => {
-      vscode.window.showInformationMessage("Code Enhance extension is active.")
-    }),
-  )
   enableGitLineHistory(context)
 }
 
